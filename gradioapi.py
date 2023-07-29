6a12f76ae73b0933cc4a02515af78c0abaed5cd4
DhirajKumar05003 <113008335+DhirajKumar05003@users.noreply.github.com>
2023-07-29 23:13:53 +0530

    Update gradioapi.py

--- gradioapi.py
+++ gradioapi.py
@@ -3,6 +3,6 @@ import gradio as gr
 import subprocess
 
 def run_streamlit_app():
-    subprocess.call(["streamlit", "run", "streamlit_app.py"])
+    subprocess.call(["streamlit", "run", "app.py"])
 
-gr.Interface(fn=run_streamlit_app).launch(share=True)
+gr.Interface(fn=run_app).launch(share=True)
